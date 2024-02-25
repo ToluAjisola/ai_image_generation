8b9340189de9d0a79ed88632f647fb4e1ece0f8c
ToluAjisola <toluwalopeajisola@gmail.com>
2024-02-24 20:55:15 -0500

    Removed block comment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,17 +3,7 @@ import React, { useEffect, useState } from "react";
 
 import { Card, FormField, Loader } from "../components";
 
-/* const RenderCards = ({ data, title }) => {
-  if (data?.length > 0) {
-    return data.map((post) =>
-      <Card key={post._id} {...post} />);
-  }
 
-  return (
-    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">
-      {title}</h2>
-  );
-}; */
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
     return data.map((post) =>
