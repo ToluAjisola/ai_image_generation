70009bce8011e5e73616b2057152e879eb118954
ToluAjisola <toluwalopeajisola@gmail.com>
2024-03-11 02:29:07 -0400

    Routes created

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 //pull environment variables from dotenv file
 dotenv.config();
@@ -12,6 +14,10 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+//middleware-api endpoints
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 //route
 app.get("/", async (req, res) => {
