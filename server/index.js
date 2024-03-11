416dc55511044b2144eed45e7db0e11aef64c2ae
ToluAjisola <toluwalopeajisola@gmail.com>
2024-03-10 23:11:31 -0400

    Started server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+//pull environment variables from dotenv file
+dotenv.config();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+//route
+app.get("/", async (req, res) => {
+  res.send("Hello from DALL.E!")
+  });
+
+
+const startServer = async () => {
+  
+    app.listen(8080, () => console.log("Server started on port http://localhost:8080"));
+  
+};
+
+startServer();
