67684c3cef8eb45decf0ed498ed47be82ddbe262
ToluAjisola <toluwalopeajisola@gmail.com>
2024-03-17 01:26:55 -0400

    Fixed Image creation error

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,33 +1,26 @@
 import express from "express";
 import * as dotenv from "dotenv";
-//import { Configuration, OpenAIApi } from "openai";
-import OpenAI from 'openai';
-
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
 
-/* const configuration = new Configuration({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration); */
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
-});
+const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from DALL-E! in dalle route" );
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
-
 
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.images.generate({
+    const aiResponse = await openai.createImage({
       prompt,
       n: 1,
       size: "1024x1024",
