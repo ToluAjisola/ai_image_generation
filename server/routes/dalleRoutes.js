92eb96e856c52913ae36f82748402c54edd33491
ToluAjisola <toluwalopeajisola@gmail.com>
2024-03-16 23:53:05 -0400

    Implemented the api retrival from openapi in dalleRoutes

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,10 +1,47 @@
 import express from "express";
 import * as dotenv from "dotenv";
 //import { Configuration, OpenAIApi } from "openai";
+import OpenAI from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
+
+/* const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration); */
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+});
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E! in dalle route" });
+});
+
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
 
 export default router;
