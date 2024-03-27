c24886bca168c6fad69c0b4e413cee8da6063b2f
ToluAjisola <toluwalopeajisola@gmail.com>
2024-03-26 21:38:25 -0400

    Changed server fetch location

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -31,7 +31,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          "http://localhost:8080/api/v1/post",
+          "https://ai-image-generation-u2p0.onrender.com/api/v1/post",
           {
             method: "POST",
             headers: {
@@ -59,7 +59,7 @@ const CreatePost = () => {
        try {
          setGeneratingImg(true);
          const response = await fetch(
-           "http://localhost:8080/api/v1/dalle",
+           "https://ai-image-generation-u2p0.onrender.com/api/v1/dalle",
            {
              method: "POST",
              headers: {
